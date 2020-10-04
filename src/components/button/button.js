ec28cf437923d664e374b71715e9d8161fe46a27
David Lecras <davidlecras@hotmail.com>
2020-10-04 17:10:14 +0200

    adding API REST

--- src/components/button/button.js
+++ src/components/button/button.js
@@ -3,11 +3,9 @@ import React from "react";
 const button = (props) => {
   const btnCss = `btn ${props.btnType} ${props.css}`;
   return (
-    <div className="container">
     <button className={btnCss} onClick={props.clic}>
       {props.children}
     </button>
-    </div>
   );
 };
 
--- src/containers/CountryManager/countryManager.js
+++ src/containers/CountryManager/countryManager.js
@@ -1,22 +1,39 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import Title from "../../components/title/title";
-import Button from "../../components/button/button"
+import Button from "../../components/button/button";
+import axios from "axios";
 
-class CountryManager extends Component{
+class CountryManager extends Component {
+  state = {
+    countryList: [],
+  };
+
+  loadCountryList= ()=>{
+    axios.get("https://restcountries.eu/rest/v2/all")
+    .then((response)=>{
+      const countryList= Object.values(response.data);
+      this.setState({countryList:countryList})
+    })
+    .catch((error)=>console.log(error))
+  }
+
+  componentDidMount=()=>{
+    this.loadCountryList()
+  }
   render() {
-    return(
+    return (
       <>
-      <Title>Liste des pays du monde</Title>
-      <Button btnType="btn-info">Tous</Button>
-      <Button btnType="btn-info">Afrique</Button>
-      <Button btnType="btn-info">Amérique</Button>
-      <Button btnType="btn-info">Asie</Button>
-      <Button btnType="btn-info">Europe</Button>
-      <Button btnType="btn-info">Océanie</Button>
-      <div>pays</div>
-      <div>pagination</div>
+        <Title>Liste des pays du monde</Title>
+        <Button btnType="btn-info">Tous</Button>
+        <Button btnType="btn-info">Afrique</Button>
+        <Button btnType="btn-info">Amérique</Button>
+        <Button btnType="btn-info">Asie</Button>
+        <Button btnType="btn-info">Europe</Button>
+        <Button btnType="btn-info">Océanie</Button>
+        <div>pays</div>
+        <div>pagination</div>
       </>
-    )
+    );
   }
 }
 export default CountryManager;
