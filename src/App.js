d139cc3ce2c248ada860c2215aa597f8c15d8e6f
David Lecras <davidlecras@hotmail.com>
2020-10-13 18:11:59 +0200

    adding navbar component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from "react";
-import CountryManager from "./containers/CountryManager/countryManager"
-
+import CountryManager from "./containers/CountryManager/countryManager";
+import { BrowserRouter as Router, Route} from "react-router-dom";
+import NavBar from "./components/navBar/navBar"
 
 function App() {
   return (
-      <CountryManager/>
+    <Router>
+    <NavBar/>
+      <Route path="/" exact render={()=><h1>Page d'accueil</h1>} />
+      <Route path="/pays" exact component={CountryManager} />
+    </Router>
   );
 }
 
